1674e325c769dc76b3b51fa13f6d50e9e6ead82b
anurag-dixit09 <anuragdixit0357@gmail.com>
2024-06-26 00:59:31 -0700

    Input & output change

---
+++ AK_02_Inputoutput.java
@@ -0,0 +1,31 @@
+package apna_collage;
+
+import java.util.Scanner;
+
+public class AK_02_Inputoutput {
+    public static void main(String[] args) {
+    // varibals
+    /*String name="tonny stark";
+    int age= 45;
+    int a = 10;
+    int b = 25;
+    int sum = a+b;
+    System.out.println(sum);
+    */
+
+    /*Scanner sc = new Scanner(System.in);
+    String name= sc.nextLine();
+    System.err.println(name);
+    */
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter first No.");
+    int a = sc.nextInt();
+    System.out.println("Enter second No.");
+    int b = sc.nextInt();
+    int sum= a+b;
+    System.out.println(sum);
+
+    }
+}
+    
+
